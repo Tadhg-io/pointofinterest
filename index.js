3149f4b64aec7e6cb8e52ccd87127f69b8b31160
Tadhg <oconghat@tcd.ie>
2021-05-23 20:45:07 +0100

    Preparation for Heroku Deployment

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ const handlebarsHelpers = require("./app/utils/handlebars-helpers");
 env.config();
 
 const server = Hapi.server({
-  port: 3000,
-  host: 'localhost',
+  port: process.env.PORT || 3000,
 });
 
 const credentials = {
